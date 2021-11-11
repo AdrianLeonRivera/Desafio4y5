e018669058aa896189978e86fd59e5a0cb0fafd2
AdrianLeonRivera <56595479+AdrianLeonRivera@users.noreply.github.com>
2021-11-11 15:07:46 +0100

    Subida Desafio 4 y 5

---
+++ Desafios/desafio3spring/src/main/java/com/example/desafio3spring/services/ClienteService.java
@@ -0,0 +1,37 @@
+package com.example.desafio3spring.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.desafio3spring.entities.Cliente;
+import com.example.desafio3spring.repository.ClienteRepositoryI;
+
+@Service
+public class ClienteService implements ClienteServiceI{
+
+	@Autowired
+	public ClienteRepositoryI repository;
+	
+	@Override
+	public Cliente findByNombreAndApellidos(String nombre, String apellidos) {
+		return repository.findByNombreAndApellidos(nombre, apellidos);
+	}
+
+	@Override
+	public void añadirCliente(Cliente cli) {
+		repository.save(cli);
+	}
+
+	@Override
+	public List mostrarCliente() {
+		return repository.findAll();
+	}
+
+	@Override
+	public void borrarCliente(Cliente cli) {
+		repository.delete(cli);
+		
+	}
+}
