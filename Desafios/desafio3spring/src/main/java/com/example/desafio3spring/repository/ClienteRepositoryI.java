f6e39deaca061ec8859bde560d6ec397bdbfca3d
AdrianLeonRivera <56595479+AdrianLeonRivera@users.noreply.github.com>
2021-11-11 17:10:45 +0100

    Delete ClienteRepositoryI.java

--- Desafios/desafio3spring/src/main/java/com/example/desafio3spring/repository/ClienteRepositoryI.java
+++
@@ -1,9 +0,0 @@
-package com.example.desafio3spring.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.desafio3spring.entities.Cliente;
-
-public interface ClienteRepositoryI extends JpaRepository<Cliente, Long>{
-	Cliente findByNombreAndApellidos(String nombre, String apellidos);
-}
